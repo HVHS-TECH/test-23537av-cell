e401c3d7e45bf2f720727da41e83ab25592a058a
23537av-cell <23537av@hvhs.school.nz>
2026-04-30 21:21:15 +0000

    Apr 30, 2026, 9:21 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -15,8 +15,6 @@ let myBirthYear = 2008;
 let myNumber = 15;
 let tenYear = 10;
 let myOthernumber = 17;
-let number = 3; 
-function writeLine(){
 console.log("As of " + userCurrentYear + " you are " + userAge);
 console.log("you have " + pocketMoney + " dollars ");
 console.log("you were born in " + myBirthYear);
@@ -30,4 +28,10 @@ OUTPUT.innerHTML += "<p> Hello " + userName + "</p>";
 OUTPUT.innerHTML += "<p> I am 17 years old .</p>";
 OUTPUT.innerHTML += "<p> I was born in 2008 .</p>";
 OUTPUT.innerHTML += "<p> Welcome to the shop .</p>";
-function writeLine();
+writeLine()
+function writeLine (){
+
+
+
+
+}
