a286e3a791400fd4e2306b95b882ae7dc982abd0
23537av-cell <23537av@hvhs.school.nz>
2026-05-15 00:01:43 +0000

    May 15, 2026, 12:01 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -37,11 +37,13 @@ function start() {
 
     function writeLine() {
     OUTPUT.innerHTML += "<p> Welcome to the shop</p>"
+    }
+    
+    const OUTPUT = document.getElementById("JavaScriptOutput");
     function getFormInput() { 
     const NAME_FIELD = document.getElementById("nameField");
     let nameUser = NAME_FIELD.value;
     OUTPUT.innerHTML += "<p> Your name is " + NAME_FIELD + "</p>";
-}
 }
 }
 
