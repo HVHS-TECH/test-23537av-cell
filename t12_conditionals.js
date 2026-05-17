8c64675f337e7672a0cea566aca1f2b1b434869b
23537av-cell <23537av@hvhs.school.nz>
2026-05-17 23:45:40 +0000

    May 17, 2026, 11:45 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -54,6 +54,7 @@ function getFormInput() {
     const Pocket_Money = document.getElementById("pocketMoney");
     let money = Pocket_Money.value;
     OUTPUT.innerHTML += "<p> You have " + money + "</p>";
+    
 }
 
 
