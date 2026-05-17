54b1b741e86f4eb1afdf5f0e9c2ee155ba6ed106
23537av-cell <23537av@hvhs.school.nz>
2026-05-17 03:30:58 +0000

    May 17, 2026, 3:30 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -46,6 +46,6 @@ function start() {}
     if (Pocket_Money >=4){
     OUTPUT.innerHTML += "<p> you CAN affrod a chocolate bar " + "<p>";
 } else{
-    OUTPUT.innerHTML +=  "<p> Sorry you Can'taffrod a chocolate bar " + "<p>";
+    OUTPUT.innerHTML +=  "<p> Sorry you Can't affrod a chocolate bar " + "<p>";
 }
 
