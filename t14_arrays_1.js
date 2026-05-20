5a02e0878573f42ff92f9a9c1640f74c5bd2066a
23537av-cell <23537av@hvhs.school.nz>
2026-05-20 22:53:16 +0000

    May 20, 2026, 10:53 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -75,6 +75,7 @@ function getFormInput() {
 }
 function getFormInput() {
     let chocolateArray = ["You loath chocolate", "Chocolate is meh" , "Chocolate is pretty good" , "Chocolate is the best thing EVER!!!!"];
+    const 
     let choice = CHOOSE_FIELD.value;
     OUTPUT.innerHTML += "<p> You chose:" + chocolateArray [choice] + "</p>";
 
