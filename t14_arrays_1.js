06d8e1e22c91b5e51d54c22f6d6c4c5fab485c2e
23537av-cell <23537av@hvhs.school.nz>
2026-05-20 23:00:38 +0000

    text

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -74,10 +74,10 @@ function getFormInput() {
     }
 }
 function getFormInput() {
-    let chocolateArray = ["You loath chocolate", "Chocolate is meh" , "Chocolate is pretty good" , "Chocolate is the best thing EVER!!!!"];
+    let messages = ["You loath chocolate", "Chocolate is meh" , "Chocolate is pretty good" , "Chocolate is the best thing EVER!!!!"];
     const CHOCOLATE_FIELD = document.getElementById("chocolateField");
     let userChoice = CHOCOLATE_FIELD.value;
-    OUTPUT.innerHTML += "<p> You chose:" + chocolateArray [userChoice] + "</p>";
+    OUTPUT.innerHTML += "<p> You chose:" + messages [userChoice] + "</p>";
 }
 
 
