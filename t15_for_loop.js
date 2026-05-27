665ac1880e2f5d62201e37e0637138b43bcef989
23537av-cell <23537av@hvhs.school.nz>
2026-05-27 22:36:44 +0000

    May 27, 2026, 10:36 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -14,10 +14,12 @@ const VERSE_FIELD = document.getElementById("verseField");
 let bottle = VERSE_FIELD.value;
 let bottles = 100
 OUTPUT.innerHTML += "";
-for(let i = 1; i < 21; i++  ){
-if (bottles === 100)
+for(let i = 1; i < 21; i-- ){
+if (bottles === 1){
+    }
     OUTPUT.innerHTML += "<p> 100 bottles of milk on the wall " + "<p>"
-    OUTPUT.innerHTML += "<p> 99 bottles of milk on the wall" + "<p> "
+    OUTPUT.innerHTML += "<p> 99 bottles of milk on the wall" + "<p> " }
+    else
 
 
 
