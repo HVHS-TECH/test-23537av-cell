8bbd38415c665d987544dafe936cc106aef7b7a6
23537av-cell <23537av@hvhs.school.nz>
2026-05-27 22:33:43 +0000

    May 27, 2026, 10:33 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -12,8 +12,10 @@ Main code
 function getFormInput() {
 const VERSE_FIELD = document.getElementById("verseField");
 let bottle = VERSE_FIELD.value;
+let bottles = 100
 OUTPUT.innerHTML += "";
 for(let i = 1; i < 21; i++  ){
+if (bottles < 100)
     OUTPUT.innerHTML += "<p> 100 bottles of milk on the wall " + "<p>"
     OUTPUT.innerHTML += "<p> 99 bottles of milk on the wall" + "<p> "
 
