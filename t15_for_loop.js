507adabfd8ac7f68470556f2cde0b64cfc669ebc
23537av-cell <23537av@hvhs.school.nz>
2026-05-27 22:50:01 +0000

    May 27, 2026, 10:50 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -10,17 +10,16 @@ console.log("Running T1.js");
 Main code
 ****************************/
 function getFormInput() {
-const VERSE_FIELD = document.getElementById("verseField");
-let bottle = VERSE_FIELD.value;
-let bottles = 100
-OUTPUT.innerHTML += "";
-for(let i = 1; i < 21; i-- ){
-if (bottles === 1){
+    const VERSE_FIELD = document.getElementById("verseField");
+    let bottle = VERSE_FIELD.value;
+    let bottles = 100
+    OUTPUT.innerHTML += "";
+    for (let i = 1; i < 21; i--) {
+        if (bottles === 1) {
+        }
+        OUTPUT.innerHTML += "<p> 100 bottles of milk on the wall " + "<p>"
+        OUTPUT.innerHTML += "<p>100 bottles of milk on the wall
     }
-    OUTPUT.innerHTML += "<p> 100 bottles of milk on the wall " + "<p>"
-    OUTPUT.innerHTML += "<p> 99 bottles of milk on the wall" + "<p> " }
-    else
-
 
 
 }
