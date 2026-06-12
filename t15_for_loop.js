aa059a9c322bcc9758c0a9db734e6c5d6b115113
23537av-cell <23537av@hvhs.school.nz>
2026-06-12 00:00:18 +0000

    Jun 12, 2026, 12:00 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -19,9 +19,10 @@ function getFormInput() {
         if (bottles === 1) {
         OUTPUT.innerHTML += "<p> 1 green bottle hanging on the wall " + "<p>"
         OUTPUT.innerHTML += "<p> And if 1 green botlle should accidentally fall " + "<p>"  
+          OUTPUT.innerHTML += "<p> There will be no green bottles hanging on the wall" + "<p>" 
          }
         else { 
-            OUTPUT.innerHTML += "<p> There will be no green bottles hanging on the wall" + "<p>"  
+            OUTPUT.innerHTML += "<p>" + "<p>"  
          }
         
         
