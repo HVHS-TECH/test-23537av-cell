204a5e87eaad3b548f8e7b5da907413602867f29
23537av-cell <23537av@hvhs.school.nz>
2026-06-12 00:04:44 +0000

    Jun 12, 2026, 12:04 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -13,16 +13,16 @@ Main code
 function getFormInput() {
     const VERSE_FIELD = document.getElementById("verseField");
     let bottle = VERSE_FIELD.value;
-    let bottles = 100
+    let bottles = 10
     OUTPUT.innerHTML += "";
     for (let i = 1; i < 21; i--) {
         if (bottles === 1) {
-        OUTPUT.innerHTML += "<p> 1 green bottle hanging on the wall " + "<p>"
+        OUTPUT.innerHTML += "<p> 10 green bottles hanging on the wall " + "<p>"
         OUTPUT.innerHTML += "<p> And if 1 green botlle should accidentally fall " + "<p>"  
-          OUTPUT.innerHTML += "<p> There will be no green bottles hanging on the wall" + "<p>" 
+          OUTPUT.innerHTML += "<p> There will be 9 green bottles hanging on the wall" + "<p>" 
          }
         else { 
-            OUTPUT.innerHTML += "<p>" + "<p>"  
+            OUTPUT.innerHTML += "<p>" + bottles + "green bottles hanging on the wall "+ <p>"  
          }
         
         
