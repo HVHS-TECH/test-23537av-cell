b2b02a6ca6377c5614424bdee0eb8a8fc5103e3b
23537av-cell <23537av@hvhs.school.nz>
2026-06-17 21:33:20 +0000

    Jun 17, 2026, 9:33 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -28,9 +28,11 @@ function getFormInput() {
     }
 }
         if (bottle <= 1 ){
-             OUTPUT.innerHTML += "<p> 10 green bottles hanging on the wall " + "<p>"
+             OUTPUT.innerHTML += "<p> green bottles hanging on the wall " + "<p>"
         }
-
+        else (bottle >= 1){
+            OUTPUT.innerHTML += "<p> green bottle hanging on the wall " + "<p>"
+        }
 
 
 
