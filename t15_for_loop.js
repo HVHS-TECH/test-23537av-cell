514f1cd357fa9af11ca6771494ce104315a40ac6
23537av-cell <23537av@hvhs.school.nz>
2026-06-19 00:09:37 +0000

    Jun 19, 2026, 12:09 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -29,7 +29,7 @@ function getFormInput() {
 }
 
 
-for (let count = 10; count > 1; count--) {
+for (let count = 10; count > 0; count--) {
     console.log(" ");
     console.log(+ count + " green bottles hanging on the wall. ");
     console.log(+ count + " green bottles hanging on the wall. ");
@@ -39,5 +39,10 @@ for (let count = 10; count > 1; count--) {
     } else {
         console.log("there will be " + (count - 1) + " green bottles hanging on the wall. ");
     }
+if (count >= 0) { 
+      console.log(" green bottle hanging on the wall. ");
+} else{ 
+     console.log(" green bottles hanging on the wall. ");
 
+}
 }
