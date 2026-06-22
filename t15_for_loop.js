35021f4e0f433aa36a019dd2db291b8ab318cac5
23537av-cell <23537av@hvhs.school.nz>
2026-06-22 01:29:42 +0000

    text

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -16,11 +16,15 @@ function getFormInput() {
     OUTPUT.innerHTML += "";
     for (let bottles = 10; bottles > 0; bottles--) {
         if (bottles <= 1) {
-            OUTPUT.innerHTML += "<p>" + (count)+ " green bottles hanging on the wall " + "<p>"
-
-            OUTPUT.innerHTML += "<p> And if 1 green botlle should accidentally fall " + "<p>"
-            OUTPUT.innerHTML += "<p> There will be "+ (count -1 ) + "green bottles hanging on the wall"+ "<p>"
+            OUTPUT.innerHTML += "<p>" + (count) + " green bottle hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (count) + " green bottle hanging on the wall " + "<p>"
+        } else {
+            OUTPUT.innerHTML += "<p>" + (count) + " green bottles hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (count) + " green bottles hanging on the wall " + "<p>"
         }
+              OUTPUT.innerHTML += "<p> And if 1 green botlle should accidentally fall " + "<p>"
+
+        OUTPUT.innerHTML += "<p> There will be " + (count - 1) + "green bottles hanging on the wall" + "<p>"
         else {
             OUTPUT.innerHTML += "<p>" + bottles + "green bottles hanging on the wall " + "<p>"
             OUTPUT.innerHTML += "<p>" + "And if 1 green botlle should accidentally fall " + "<p>"
@@ -36,25 +40,25 @@ function getFormInput() {
 
 for (let count = 10; count > 0; count--) {
     console.log(" ");
-    if ( count <= 1){
-    console.log(+ count + " green bottle hanging on the wall. ");
-     console.log(+ count + " green bottle hanging on the wall. ");
-} else { 
-     console.log(+ count + " green bottles hanging on the wall. ");
-      console.log(+ count + " green bottles hanging on the wall. ");
-}
-    console.log("And if one green bottle should accidentally fall. ");
-    if (count <= 2 ){
-    console.log("there will be "+ (count - 1) + " green bottle hanging on the wall ");
-}else { 
-    console.log("there will be "+ (count - 1) + " green bottles hanging on the wall ");}
-    if ( count - 1 === 0) {
-           console.log("there will be no green bottles hanging on the wall ");
-        } else if ( count - 1  ) {
-             console.log("there will be 1 green bottle hanging on the wall ");
+    if (count <= 1) {
+        console.log(+ count + " green bottle hanging on the wall. ");
+        console.log(+ count + " green bottle hanging on the wall. ");
     } else {
-            console.log("there will be "+ (count - 1) + " green bottles hanging on the wall ");
+        console.log(+ count + " green bottles hanging on the wall. ");
+        console.log(+ count + " green bottles hanging on the wall. ");
+    }
+    console.log("And if one green bottle should accidentally fall. ");
+    if (count <= 2) {
+        console.log("there will be " + (count - 1) + " green bottle hanging on the wall ");
+    } else {
+        console.log("there will be " + (count - 1) + " green bottles hanging on the wall ");
+    }
+    if (count - 1 === 0) {
+        console.log("there will be no green bottles hanging on the wall ");
+    } else if (count - 1) {
+        console.log("there will be 1 green bottle hanging on the wall ");
+    } else {
+        console.log("there will be " + (count - 1) + " green bottles hanging on the wall ");
     }
 }
 
-    
