3e33ee6bf68c13127b07bcb5cca426cbf90fe4e9
23537av-cell <23537av@hvhs.school.nz>
2026-06-22 00:08:25 +0000

    Jun 22, 2026, 12:08 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -31,15 +31,12 @@ function getFormInput() {
 
 for (let count = 10; count > 0; count--) {
     console.log(" ");
+    console.log(+ count + " green bottle hanging on the wall. ");
+    console.log(+ count + " green bottle hanging on the wall. ");
     console.log("And if one green botlle should accidentally fall. ");
-    if (count <= 1) {
-        console.log("there will be " + (count - 1) + " green bottle hanging on the wall. ");
-    } else {
-        console.log("there will be " + (count - 1) + " green bottles hanging on the wall. ");
-    }
-    if ( count <= 2 ){
-        console.log(+ count + " green bottles hanging on the wall. ");
-    } else { console.log(+ count + " green bottle hanging on the wall. ");
-
-    }
+if ( count <= 1){
+    console.log(+ count + " green bottles hanging on the wall. ");
+} else { 
+     console.log(+ count + " green bottle hanging on the wall. ");
+}
 }
