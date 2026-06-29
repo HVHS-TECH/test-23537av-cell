e67ea0b586fb0fd1b2a0b873ddee0db712945b63
23537av-cell <23537av@hvhs.school.nz>
2026-06-29 01:27:15 +0000

    text

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -63,7 +63,7 @@ for (let count = 10; count > 0; count--) {
     } else if (count - 1 === 1) {
         console.log("there will be 1 green bottle hanging on the wall ");                             
     } else {
-        console.log("there will be " + (count - 1) + " green bottles hanging on the wall ");
+        console.log("there will be " + (count - 1) + " green bottles hanging on the wall ");gvf
     }
 }
 
