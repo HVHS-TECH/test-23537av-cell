03919d4afea1a4d8024e46fa8995472b47e7d5c8
23537av-cell <23537av@hvhs.school.nz>
2026-07-01 22:12:28 +0000

    Jul 1, 2026, 10:12 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -8,17 +8,19 @@ console.log("for loops 54.js");
 //variables
 
 /****************************
+ main code
+ ****************************/
 function getFormInput() {
     const VERSE_FIELD = document.getElementById("verseField");
     let bottle = VERSE_FIELD.value;
     OUTPUT.innerHTML += "";
     for (let bottles = 10; bottles > 0; bottles--) {
         if (bottles <= 1) {
-            OUTPUT.innerHTML += "<p>" + (count) + " green bottle hanging on the wall " + "<p>"
-            OUTPUT.innerHTML += "<p>" + (count) + " green bottle hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (bottles) + " green bottle hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (bottles) + " green bottle hanging on the wall " + "<p>"
         } else {
-            OUTPUT.innerHTML += "<p>" + (count) + " green bottles hanging on the wall " + "<p>"
-            OUTPUT.innerHTML += "<p>" + (count) + " green bottles hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (bottles) + " green bottles hanging on the wall " + "<p>"
+            OUTPUT.innerHTML += "<p>" + (bottles) + " green bottles hanging on the wall " + "<p>"
         }
         OUTPUT.innerHTML += "<p> And if 1 green botlle should accidentally fall " + "<p>"
 
@@ -27,12 +29,11 @@ function getFormInput() {
         } else if (count - 1 === 1) {
             OUTPUT.innerHTML += "<p> There will be 1 green bottle hanging on the wall." + "<p>"
         } else {
-            "There will be " + (count - 1) + " green bottles hanging on the wall."
+            "There will be " + (bottles- 1) + " green bottles hanging on the wall."
         }
 }
 }
 
-****************************/
 
 
 
@@ -40,6 +41,13 @@ function getFormInput() {
 
 
 
+
+
+
+
+
+
+/****************************
 for (let count = 10; count > 0; count--) {
 
 
@@ -64,3 +72,4 @@ for (let count = 10; count > 0; count--) {
         console.log("There will be " + (count - 1) + " green bottles hanging on the wall.");
     }
 }
+****************************/
